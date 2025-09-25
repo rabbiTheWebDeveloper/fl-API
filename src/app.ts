99d42a94c44b6174455bdd807cc0f9a80f7076bd
S M AL RABBI <rabbithedeveloper@gmail.com>
2025-09-25 19:23:48 +0600

    npm package updating

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,8 @@ import cors from "cors";
 import "dotenv/config";
 import { dbConnection } from "./app/utlis/dbConnection";
 import productRoute from "./app/modules/Product/product.router";
-import userRoute from "./app/modules/User/user.router";
 import bannerRoute from "./app/modules/Banner/banner.router";
 import blogRoute from "./app/modules/Blog/blog.router";
-import orderRoute from "./app/modules/Order/order.router";
-import reviewRoute from "./app/modules/Review/review.router";
-import contactRoute from "./app/modules/ContactUs/contactus.router";
 import settingRoute from "./app/modules/Settings/settings.router";
 import categorysRoute from "./app/modules/Category/category.router";
 import globalErrorHandler from "./app/middleware/globalErrorHandler";
@@ -36,17 +32,11 @@ dbConnection();
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-
 app.use("/api/v1/product", productRoute);
 app.use("/api/v1/category", categorysRoute);
-app.use("/api/v1", userRoute);
 app.use("/api/v1", bannerRoute);
 app.use("/api/v1", blogRoute);
-app.use("/api/v1/order", orderRoute);
-app.use("/api/v1/review", reviewRoute);
-app.use("/api/v1/contact", contactRoute);
 app.use("/api/v1", settingRoute);
-
 app.use(globalErrorHandler);
 
 export default app;
