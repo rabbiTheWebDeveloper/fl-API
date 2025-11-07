4e6d39572b483c680044d801428ee356fee69a8b
S M AL RABBI <rabbithedeveloper@gmail.com>
2025-11-07 22:19:15 +0600

    updating

--- dist/app/modules/Category/category.router.js
+++ dist/app/modules/Category/category.router.js
@@ -8,6 +8,6 @@ const category_controller_1 = require("./category.controller");
 const imageUpload_1 = __importDefault(require("../../middleware/imageUpload"));
 const router = (0, express_1.Router)();
 router.post("/create", imageUpload_1.default.single("image"), category_controller_1.addCategory);
-router.delete("/create-delete/:id", category_controller_1.deleteCategory);
+router.post("/create-delete/:id", category_controller_1.deleteCategory);
 router.patch("/update/:id", imageUpload_1.default.single("image"), category_controller_1.updateCategory);
 exports.default = router;
--- src/app/modules/Category/category.router.ts
+++ src/app/modules/Category/category.router.ts
@@ -4,7 +4,7 @@ import { auth } from "../../middleware/AuthVerifyMiddleware";
 import imageUpload from "../../middleware/imageUpload";
 const router: Router = Router();
 router.post("/create", imageUpload.single("image"), addCategory);
-router.delete("/create-delete/:id", deleteCategory);
+router.post("/create-delete/:id", deleteCategory);
 router.patch("/update/:id",imageUpload.single("image"), updateCategory);
 
 export default router;
